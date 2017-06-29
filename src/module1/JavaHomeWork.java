46f93e0b8c5298b308f255f6ea9982b326d2e114
Molotow121 <molotow121@gmail.com>
2017-06-29 20:09:33 +0300

    Comlete HomeWork (I hope)

---
+++ src/module1/JavaHomeWork.java
@@ -0,0 +1,11 @@
+package module1;
+
+/**
+ * Created by molot on 28.06.2017.
+ */
+public class JavaHomeWork {
+        public  static void main(String[]args){
+            System.out.println("Work, sleep, reproduce.");
+        }
+    }
+
